61454524a4c6767d5f8f5b628f2d3d839d76f12b
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2024-01-10 15:31:09 -0500

    change2

--- Desktop_app1.py
+++ Desktop_app1.py
@@ -1,2 +1,21 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow #only need these 2 methods, not all of QtWidgets
+from PyQt5.QtGui import QIcon 
+"""
+from PyQt5 import QtWidgets """
+
+def window():
+    app = QApplication(sys.argv)
+   # no need for QtWidgets.qapp, b/c we imported from submodule. -->  app = QtWidgets.QApplication(sys.argv)
+    win = QMainWindow() #create a window
+   # win = QtWidgets.QMainWindow() #create a window
+    win.setGeometry(1200,500,500,500)
+    win.setWindowTitle("Bugatti")
+    win.setWindowIcon(QIcon("Bugattichiron1.jpg"))
+    win.setToolTip("Just drive me")
+    win.show() #show created desktop window
+    sys.exit(app.exec_()) #send argument to close app when close button clicked
+
+window() #call function 
+
+
